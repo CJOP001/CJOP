d2c8cdccebc64fd81fb859973e4d478180b2077e
OowanoO <zhenyaowan@gmail.com>
2023-09-12 22:22:51 +0800

    the design still fixing

--- App.tsx
+++ App.tsx
@@ -3,35 +3,19 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
 import AddingPost from "./screens/AddingPost";
+import AppNavigator from "./components/Navigator";
 import IOSBottomBar5Tabs from "./components/IOSBottomBar5Tabs";
 import AnnouncementPost from "./screens/AnnouncementPost";
 import CreatePost from "./screens/CreatePost";
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { View, Text, Pressable, TouchableOpacity } from "react-native";
+import { View, Text, Pressable, TouchableOpacity, Button } from "react-native";
 
 const App = () => {
   const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
 
   return (
-    <>
-      <NavigationContainer>
-        {hideSplashScreen ? (
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen
-              name="AnnouncementPost"
-              component={AnnouncementPost}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="CreatePost"
-              component={CreatePost}
-              options={{ headerShown: false }}
-            />
-          </Stack.Navigator>
-        ) : null}
-      </NavigationContainer>
-    </>
+  <AppNavigator/>
   );
 };
 export default App;
