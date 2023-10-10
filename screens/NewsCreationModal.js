6ccd68398837c9d9abcca35aa76c2abf71b89804
lwk1334 <kangwengliang6818@gmail.com>
2023-10-10 22:00:31 +0800

    Update package

--- screens/NewsCreationModal.js
+++ screens/NewsCreationModal.js
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const NewsCreation = () => {
+const NewsCreationModal = () => {
   return (
     <View>
-      <Text>NewsCreation</Text>
+      <Text>NewsCreationModal</Text>
     </View>
   )
 }
 
-export default NewsCreation
+export default NewsCreationModal
 
 const styles = StyleSheet.create({})
