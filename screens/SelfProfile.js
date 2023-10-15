d2e8d7d3d7153c29a744aaedecb04cf117d27b5f
Ho-Qi-Liang <j20032174@student.newinti.edu.my>
2023-10-16 04:56:21 +0800

    Commit backup due to issues on pc

--- screens/SelfProfile.js
+++ screens/SelfProfile.js
@@ -14,7 +14,69 @@ import { Appbar, Avatar, Searchbar, Card } from "react-native-paper";
 import { categories } from "../components/categories";
 import { dummyArticles } from "../components/articles";
 import ArticleCard from "../components/ArticleCard";
+import { launchImageLibrary } from "react-native-image-picker";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
+const getData = async () => {
+  const { data, error, count } = await supabase
+    .from("app_users")
+    .select(
+      `
+    user_image,
+    fullname,
+    `
+    )
+    .eq("id", userID);
+  SetUser(data);
+};
+
+useEffect(() => {
+  async function downloadImage(path) {
+    try {
+      const { data, error } = await supabase.storage
+        .from("UserImage")
+        .download(path);
+      if (error) {
+        throw error;
+      }
+
+      const url = URL.createObjectURL(data);
+      setAvatarUrl(url);
+    } catch (error) {
+      console.log("Error downloading image: ", error);
+    }
+  }
+
+  if (url) downloadImage(url);
+}, [url, supabase]);
+
+const uploadAvatar = async (event) => {
+  try {
+    setUploading(true);
+
+    if (!event.target.files || event.target.files.length === 0) {
+      throw new Error("You must select an image to upload.");
+    }
+
+    const file = event.target.files[0];
+    const fileExt = file.name.split(".").pop();
+    const filePath = `${uid}-${Math.random()}.${fileExt}`;
+
+    let { error: uploadError } = await supabase.storage
+      .from("UserImage")
+      .upload(filePath, file);
+
+    if (uploadError) {
+      throw uploadError;
+    }
+
+    onUpload(filePath);
+  } catch (error) {
+    alert("Error uploading avatar!");
+  } finally {
+    setUploading(false);
+  }
+};
 const renderItem = ({ item }) => {
   return <ArticleCard {...item} />;
 };
@@ -22,6 +84,7 @@ const follow = ({}) => {};
 const SelfProfile = () => {
   return (
     <View>
+      {/*Header Section*/}
       <Appbar.Header style={{ backgroundColor: "#72E6FF" }}>
         <View style={styles.customBackAction}>
           <Appbar.BackAction
@@ -31,22 +94,27 @@ const SelfProfile = () => {
             }}
           />
         </View>
-        <Appbar.Content title="Profile" style={styles.appContent} />
+        {/*Title Bar*/}
+        <Appbar.Content title="Your Profile" style={styles.appContent} />
       </Appbar.Header>
       <View>
+        {/*Profile Background*/}
         <Card mode="outlined" style={styles.profile}>
           <Card.Cover
             source={require("../assets/background.png")}
             style={{ height: 125, minWidth: "100%" }}
           />
+          {/*Profile Image*/}
           <Card.Title
             left={(props) => (
               <Avatar.Image
-                source={require("../assets/avatar.png")}
+                source={
+                  require("../assets/profile.png") /* TODO: Eventually change to depend on database*/
+                }
                 size={80}
                 style={{ transform: [{ translateY: -20 }] }}
               />
-            )}
+            )} /*Edit Profile button*/
             right={(props) => (
               <Button
                 title="Edit Profile"
@@ -61,7 +129,9 @@ const SelfProfile = () => {
           />
           <Card.Content style={{ margin: 5, transform: [{ translateY: -15 }] }}>
             <Text>Profile</Text>
+            {/*Placeholder Profile Nickname*/}
             <Text>@Profile</Text>
+            {/*Placeholder Profile Name*/}
           </Card.Content>
           <Card.Content
             style={{
@@ -70,6 +140,7 @@ const SelfProfile = () => {
             }}
           >
             <Text>A long description</Text>
+            {/*Placeholder description*/}
           </Card.Content>
           <Card.Content>
             <View style={styles.container}>
@@ -84,7 +155,9 @@ const SelfProfile = () => {
             style={{ transform: [{ translateX: 20 }], flexDirection: "row" }}
           >
             <Text>Following:</Text>
+            {/*TODO: Append following count*/}
             <Text>Followers:</Text>
+            {/*TODO: Append Follower count*/}
           </Card.Content>
         </Card>
         {/* List of articles */}
