30be5540f6369f118e8ea04331282168f49f6c46
lwk1334 <kangwengliang6818@gmail.com>
2023-11-20 23:15:06 +0800

    Update Payment.js

--- screens/Payment/Payment.js
+++ screens/Payment/Payment.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Dimensions, ScrollView, Image, RefreshControl, SafeAreaView } from 'react-native';
-import { Appbar, Button, Card, Menu } from 'react-native-paper';
+import { Appbar, Button, Card, Menu, List } from 'react-native-paper';
 import { Tab, TabView } from '@rneui/themed';
 import supabase from '../../supabase/supabase';
 import PaymentModal from './Payment_Modal';
@@ -20,7 +20,7 @@ const Payment = ({ navigation }) => {
   const [isMenuVisible, setMenuVisible] = useState(false); // State for the menu
   const [isTransferModalVisible, setTransferModalVisible] = useState(false);
   const [isWithdrawModalVisible, setWithdrawModalVisible] = useState(false);
-
+  const [isLearnAboutCreditsExpanded, setLearnAboutCreditsExpanded] = useState(false);
 
   // Constants
   const currentUserID = '1d93bd48-5c9e-43f0-9866-c0cd6a284a39'; //lwk
@@ -29,6 +29,7 @@ const Payment = ({ navigation }) => {
   const TRANSACTION_RELOAD = 'Reload';
   const TRANSACTION_RECEIVED = 'Received';
   const TRANSACTION_POST = 'Post';
+  const TRANSACTION_WITHDRAW = 'Withdraw';
 
 // Function to fetch balance
 const fetchBalance = async () => {
@@ -99,27 +100,6 @@ const handleConfirmReload = (amount) => {
   toggleModal();
 };
 
-/*const handleReload = async (amount) => {
-  try {
-    // Perform the reload operation here (e.g., send a payment request to a payment gateway).
-    // If the reload is successful, call the update and insert functions.
-    const reloadSuccessful = await performReloadOperation(amount);
-
-    if (reloadSuccessful) {
-      // Call the update function to update the credit balance.
-      await handleUpdateCreditAmount(amount);
-
-      // Call the insert function to log the credit transaction.
-      await handleInsertCreditTransaction(amount);
-    } else {
-      // Handle the case where the reload was not successful.
-      console.error('Reload operation failed.');
-    }
-  } catch (error) {
-    console.error('Error handling reload:', error);
-  }
-};*/
-
 // Function to handle menu icon press
 const handleMenuPress = () => {
   setMenuVisible(true);
@@ -146,14 +126,43 @@ const handleMenuOptionPress = (option) => {
   handleMenuClose();
 };
 
-
+const LearnAboutCredits = () => {
+  return (
+    <List.Accordion
+      title="Learn About Credits"
+      titleStyle={{ fontSize: 25, fontWeight: 'bold', color: isLearnAboutCreditsExpanded ? '#72E6FF' : '#000' }}
+      expanded={isLearnAboutCreditsExpanded}
+      onPress={() => setLearnAboutCreditsExpanded(!isLearnAboutCreditsExpanded)}
+    >
+      <List.Item title="Credit Value Guide" titleStyle={{ fontSize: 18 }} />
+      <List.Item title="1 credit = RM0.01" />
+      <List.Item title="10 credits = RM0.10" />
+      <List.Item title="100 credits = RM1" />
+      <List.Item title="1000 credits = RM10" />
+      <Text style={styles.learnText}>
+        Understanding the value of your credits empowers you. Your wallet, your choices! 💸
+      </Text>
+    </List.Accordion>
+  );
+};
 
   const renderContent = () => {
+    const spentTransactions = creditTransactions.filter(
+      (item) => item.transaction_type === TRANSACTION_RELOAD || item.transaction_type === TRANSACTION_POST
+    );
+  
+    const receivedTransactions = creditTransactions.filter(
+      (item) => item.transaction_type === TRANSACTION_RECEIVED
+    );
+  
+    const withdrawTransactions = creditTransactions.filter(
+      (item) => item.transaction_type === TRANSACTION_WITHDRAW
+    );
     return (
       <View>
         {index === 0 && (
           <View style={{ flex: 1 }}>
-            {creditTransactions.map((item, index) => (
+            {spentTransactions.map((item, index) => (
               <Card key={index} style={{ marginVertical: 10 }}>
                 <Card.Content style={styles.cardContainer}>
                   <Image
@@ -174,21 +183,133 @@ const handleMenuOptionPress = (option) => {
         )}
         {index === 1 && (
           <View>
-            <Text> No Received History</Text>
+            {receivedTransactions.length > 0 ? (
+            receivedTransactions.map((item, index) => (
+              <Card key={index} style={{ marginVertical: 10 }}>
+                <Card.Content style={styles.cardContainer}>
+                  <Image
+                    source={require('../../assets/credit-card.png')}
+                    style={styles.icon}
+                  />
+                  <View style={styles.infoContainer}>
+                    <Text style={styles.transactionType}> {item.transaction_type}</Text>
+                    <Text style={styles.dateText}> {new Date(item.date).toLocaleString('en-US', { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true })}</Text>
+                  </View>
+                  <Text style={[styles.amountText, { color: item.transaction_type === TRANSACTION_RELOAD ? 'green' : item.transaction_type === TRANSACTION_POST ? '#FF0000' : 'black' }]}> 
+                    {item.amount} credits
+                  </Text>
+                </Card.Content>
+              </Card>
+            ))
+          ) : (
+            <Text>No Received History</Text>
+          )}
           </View>
         )}
         {index === 2 && (
           <View>
-            <Text> No Withdraw History</Text>
-            {/* Add your content for the "Withdraw" tab here */}
+            {withdrawTransactions.length > 0 ? (
+            withdrawTransactions.map((item, index) => (
+              <Card key={index} style={{ marginVertical: 10 }}>
+                <Card.Content style={styles.cardContainer}>
+                  <Image
+                    source={require('../../assets/credit-card.png')}
+                    style={styles.icon}
+                  />
+                  <View style={styles.infoContainer}>
+                    <Text style={styles.transactionType}> {item.transaction_type}</Text>
+                    <Text style={styles.dateText}> {new Date(item.date).toLocaleString('en-US', { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true })}</Text>
+                  </View>
+                  <Text style={[styles.amountText, { color: item.transaction_type === TRANSACTION_RELOAD ? 'green' : item.transaction_type === TRANSACTION_POST ? '#FF0000' : 'black' }]}> 
+                    {item.amount} credits
+                  </Text>
+                </Card.Content>
+              </Card>
+            ))
+          ) : (
+            <Text>No Withdraw History</Text>
+          )}
           </View>
         )}
       </View>
     );
   };
 
+  // Function to handle transfer
+  const handleTransfer = async (recipientID, transferAmount) => {
+    try {
+      // Check if the recipient ID is valid (you may want to add more validation)
+      const recipientExists = await supabase
+        .from('credits')
+        .select('user_id')
+        .eq('user_id', recipientID)
+        .single();
+
+      if (!recipientExists) {
+        console.error('Recipient not found');
+        return;
+      }
+
+      // Deduct the transfer amount from the current user's balance
+      const updatedSenderBalance = balance - transferAmount;
+      setBalance(updatedSenderBalance);
+
+      // Add the transfer amount to the recipient's balance
+      const { data: recipientData, error: recipientError } = await supabase
+        .from('credits')
+        .upsert([
+          {
+            user_id: recipientID,
+            credit_amount: supabase.sql`credit_amount + ${transferAmount}`,
+          },
+        ]);
+
+      if (recipientError) {
+        console.error('Error updating recipient balance:', recipientError);
+        // Handle error (rollback sender's balance update, show error message, etc.)
+        return;
+      }
+
+      // Create a credit transaction for the sender
+      const senderTransaction = {
+        user_id: currentUserID,
+        transaction_type: TRANSACTION_TRANSFER,
+        amount: -transferAmount,
+        date: new Date(),
+      };
+
+      // Create a credit transaction for the recipient
+      const recipientTransaction = {
+        user_id: recipientID,
+        transaction_type: TRANSACTION_RECEIVED,
+        amount: transferAmount,
+        date: new Date(),
+      };
+
+      // Insert both transactions into the credit_transactions table
+      const { error: transactionError } = await supabase
+        .from('credit_transactions')
+        .upsert([senderTransaction, recipientTransaction]);
+
+      if (transactionError) {
+        console.error('Error creating credit transactions:', transactionError);
+        // Handle error (rollback transactions, show error message, etc.)
+        return;
+      }
+
+      // Notify the user that the transfer was successful
+      console.log('Transfer successful');
+
+      // Close the transfer modal or perform any other necessary actions
+      setTransferModalVisible(false);
+    } catch (error) {
+      console.error('Error during transfer:', error);
+      // Handle generic error (show error message, rollback changes, etc.)
+    }
+  };
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Appbar.Header style={{ backgroundColor: '#72E6FF' }}>
         <View style={styles.customBackAction}>
           <Appbar.BackAction
@@ -215,6 +336,11 @@ const handleMenuOptionPress = (option) => {
       </Appbar.Header>
 
       <View style={{ flex: 1, padding: 15 }}>
+        <RefreshControl
+            refreshing={isRefreshing}
+            onRefresh={handleRefresh}
+            colors={['#72E6FF']} // Customize the color of the refresh spinner
+          />
         <Text style={{ fontSize: 20, textAlign: 'center', padding: 15 }}>
           Balance
         </Text>
@@ -237,7 +363,13 @@ const handleMenuOptionPress = (option) => {
         />
 
         {/* Transfer Modal */}
-      <TransferModal visible={isTransferModalVisible} onClose={() => setTransferModalVisible(false)} />
+        <TransferModal
+          visible={isTransferModalVisible}
+          onClose={() => setTransferModalVisible(false)}
+          onTransfer={(recipientID, transferAmount) =>
+            handleTransfer(recipientID, transferAmount)
+          }
+        />
 
         {/* Withdraw Modal */}
       <WithdrawModal
@@ -247,9 +379,8 @@ const handleMenuOptionPress = (option) => {
         onUpdateBalance={(newBalance) => setBalance(newBalance)}  // Pass a function to update the balance
       />
 
-        <Text style={{ textAlign: 'center', color: '#72E6FF', padding: 10, fontSize: 25, fontWeight: 'bold' }}>
-          Learn About Credits
-        </Text>
+      {/* Learn About Credits Section */}
+      <LearnAboutCredits />
 
 
         <View style={{ flexDirection: 'row', width: '100%', alignItems: 'center' }}>
@@ -278,21 +409,23 @@ const handleMenuOptionPress = (option) => {
           </Tab>
         </View>
 
-        <ScrollView
-          style={{ flex: 1 }}
-          refreshControl={
-            <RefreshControl
-              refreshing={isRefreshing}
-              onRefresh={handleRefresh}
-              colors={['#72E6FF']} // Customize the color of the refresh spinner
-            />
-          }
-        >
-          {renderContent()}
-        </ScrollView>
+        <View style={{ flex: 1 }}>
+          <ScrollView
+            style={{ flex: 1 }}
+            refreshControl={
+              <RefreshControl
+                refreshing={isRefreshing}
+                onRefresh={handleRefresh}
+                colors={['#72E6FF']} 
+              />
+            }
+          >
+            {renderContent()}
+          </ScrollView>
+        </View>
 
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -350,5 +483,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginLeft: 20,
     fontWeight: 'bold', 
+  },
+  learnText: {
+    fontSize: 16,
+    textAlign: 'left',
+    color: '#333', // Adjust the color as needed
+    lineHeight: 24,
   },
 });
