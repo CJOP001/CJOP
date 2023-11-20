555f50b170c4a9d34298cf4c8dd91f0f0ee32460
lwk1334 <kangwengliang6818@gmail.com>
2023-11-20 23:15:26 +0800

    Update WithdrawModal.js

--- screens/Payment/WithdrawModal.js
+++ screens/Payment/WithdrawModal.js
@@ -1,20 +1,22 @@
 // WithdrawModal.js
 import React, { useState } from 'react';
-import { Modal, View, Text, StyleSheet} from 'react-native';
+import { Modal, View, Text, StyleSheet, Alert} from 'react-native';
 import { Button } from 'react-native-paper';
 import supabase from '../../supabase/supabase';
 
 const reloadOptions = [10, 15, 20, 50, 100, 500];
 const currentUserID = '1d93bd48-5c9e-43f0-9866-c0cd6a284a39';
 
-const WithdrawModal = ({ visible, onClose, balance, onUpdateBalance }) => {
-  // Add your withdraw modal content and logic here
+const WithdrawModal = ({ visible, onClose, balance }) => {
+  
   const [selectedAmount, setSelectedAmount] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
 
 const handleWithdrawal = async () => {
   setLoading(true);
+  setError(null);
   
   try {
     console.log('Current balance:', balance);
@@ -24,22 +26,44 @@ const handleWithdrawal = async () => {
       console.log(`Withdrawal confirmed with amount: $${selectedAmount}`);
       const newBalance = balance - selectedAmount;
       
-      // Simulate an asynchronous database update
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      
       // Update the user's balance in the database
-      await onUpdateBalance(newBalance);
+      const { error: updateError } = await supabase
+        .from('credits')
+        .update({ credit_amount: newBalance })
+        .eq('user_id', currentUserID);
+
+        if (updateError) {
+          throw updateError;
+        }
+
+      // Insert a new row into the credit_transactions table
+      const { error: insertError } = await supabase
+        .from('credit_transactions')
+        .insert([
+          {
+            user_id: currentUserID,
+            amount: selectedAmount,
+            transaction_type: 'Withdraw',
+            date: new Date().toISOString(),
+          },
+        ]);
+
+      if (insertError) {
+        throw insertError;
+      }
+      
       
       onClose(); // Close the modal after withdrawal
     } else {
       console.log('Insufficient balance for withdrawal');
-      // Show an alert or any UI indication for insufficient balance
+      Alert.alert('Insufficient Balance', 'You do not have enough credits to withdraw.');
     }
   } catch (error) {
     console.error('Error handling withdrawal:', error);
     // Handle the error, show an error message to the user, or retry the operation
   } finally {
     setLoading(false);
+    setSelectedAmount(null);
   }
 };
 
@@ -47,71 +71,58 @@ const handleWithdrawal = async () => {
     <Modal transparent={true} animationType="fade" visible={visible}>
       <View style={styles.modalContainer}>
         <View style={styles.modalContent}>
-          <Text style={styles.modalTitle}>Withdraw Credits</Text>
+            <Text style={styles.modalTitle}>Withdraw Credits</Text>
             <Text style={styles.modalDescription}>
               Select the amount you want to withdraw from your account.
             </Text>
           <View style={styles.buttonGrid}>
-            {reloadOptions.map((amount) => (
-              <Button
-                key={amount}
-                mode='contained' 
-                onPress={() => setSelectedAmount(amount)}
-                style={[
-                  styles.modalButton,
-                  {
-                    width: '30%',
-                    height:'40%',
-                    backgroundColor: selectedAmount === amount ? '#72E6FF' : '#E9EFF7',
-                  }, 
-                ]}
-                uppercase={false}
-                disabled={selectedAmount === amount}
-                labelStyle={styles.amountText}
-                
-              >
-                {amount}
-              </Button>
-            ))}
+              {reloadOptions.map((amount) => (
+                <Button
+                  key={amount}
+                  mode='contained' 
+                  onPress={() => setSelectedAmount(amount)}
+                  style={[
+                    styles.modalButton,
+                    {
+                      width: '30%',
+                      height:'40%',
+                      backgroundColor: selectedAmount === amount ? '#72E6FF' : '#E9EFF7',
 
-          <Button
-            mode="contained"
-            onPress={handleWithdrawal}
-            style={styles.modalButton}
-            contentStyle={styles.buttonContent}
-            disabled={selectedAmount === null}
-            loading={loading}
-          >
-              Withdraw
-          </Button>
-          
-          <Button
-            mode="contained"
-            onPress={() => {
-              setSelectedAmount(null);
-              onClose(); 
-            }}
-            style={styles.modalButton}
-            contentStyle={styles.buttonContent}
-          >
-            Cancel
-          </Button>
+                    }, 
+                  ]}
+                  uppercase={false}
+                  disabled={selectedAmount === amount}
+                  labelStyle={styles.amountText}
+                  accessibilityLabel={`Select ${amount} credits`}
+                >
+                  {amount}
+                </Button>
+              ))}
+
+            <Button
+              mode="contained"
+              onPress={handleWithdrawal}
+              style={styles.modalButton}
+              contentStyle={styles.buttonContent}
+              disabled={selectedAmount === null}
+              loading={loading}
+            >
+                Withdraw
+            </Button>
+            
+            <Button
+              mode="contained"
+              onPress={() => {
+                setSelectedAmount(null);
+                onClose(); 
+              }}
+              style={styles.modalButton}
+              contentStyle={styles.buttonContent}
+            >
+              Cancel
+            </Button>
           </View>
 
-          {/*<ScrollView>
-            {reloadOptions.map((amount, index) => (
-              <View key={index} style={styles.radioButtonContainer}>
-                <RadioButton
-                  value={amount}
-                  status={selectedAmount === amount ? 'checked' : 'unchecked'}
-                  onPress={() => setSelectedAmount(amount)}
-                />
-                <Text>{`$${amount}`}</Text>
-              </View>
-            ))}
-            </ScrollView>*/}
-
-          
 
         </View>
       </View>
@@ -166,8 +177,8 @@ const styles = StyleSheet.create({
     alignItems: 'center', 
   },
   buttonContent: {
-    height: 'auto', // Set height to 'auto' for text wrapping
-    paddingHorizontal: 12, // Adjust horizontal padding as needed
+    height: 'auto', 
+    paddingHorizontal: 12, 
   },
   cancelButton: {
     flex: 1, 
