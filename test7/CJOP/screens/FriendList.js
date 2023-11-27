953e5e7db0eb8fcea598a984f04e700329dac12e
lwk1334 <kangwengliang6818@gmail.com>
2023-11-27 18:22:13 +0800

    Create FriendList.js

---
+++ test7/CJOP/screens/FriendList.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FriendList = () => {
+  return (
+    <View>
+      <Text>FriendList</Text>
+    </View>
+  )
+}
+
+export default FriendList
+
+const styles = StyleSheet.create({})
