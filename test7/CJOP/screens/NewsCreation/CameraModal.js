c7d511bbcb7412e665950d524ea2a2d7cde93581
lwk1334 <kangwengliang6818@gmail.com>
2023-11-27 18:21:59 +0800

    Create CameraModal.js

---
+++ test7/CJOP/screens/NewsCreation/CameraModal.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CameraModal = () => {
+  return (
+    <View>
+      <Text>CameraModal</Text>
+    </View>
+  )
+}
+
+export default CameraModal
+
+const styles = StyleSheet.create({})
